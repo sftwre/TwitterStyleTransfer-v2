6c2fc031c29f40733976ec02fed8a1aa1b6458ca
Isaac Buitrago <isaacb@arlut.utexas.edu>
2020-11-19 22:52:31 -0600

    Seperated model into encoder and decoder subclasses of keras.Model class

--- model.py
+++ model.py
@@ -17,27 +17,34 @@ class Sampling(layers.Layer):
 
 latent_dim = 2
 
-encoder_inputs = keras.Input(shape=(28, 28, 1))
-x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
-x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
-x = layers.Flatten()(x)
-x = layers.Dense(16, activation="relu")(x)
-z_mean = layers.Dense(latent_dim, name="z_mean")(x)
-z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
-z = Sampling()([z_mean, z_log_var])
-encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
-encoder.summary()
 
+class Encoder(keras.Model):
+    """
+    Unidirectional RNN encoder
+    """
+    def __init__(self, embedder, **kwargs):
+        super(Encoder, self).__init__(**kwargs)
+        self.model = keras.Sequential()
+        self.gru = layers.GRU(700, dropout=0.5)
+        self.model.add(embedder)
+        self.model.add(self.gru)
 
-latent_inputs = keras.Input(shape=(latent_dim,))
-x = layers.Dense(7 * 7 * 64, activation="relu")(latent_inputs)
-x = layers.Reshape((7, 7, 64))(x)
-x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
-x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
-decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
-decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
-decoder.summary()
+    def call(self, inputs):
+        x = self.model(inputs)
+        return x
 
+class Decoder(keras.Model):
+    """
+    LSTM based generator that re-creates text based on
+    latent space and controllable parameters.
+    """
+    def __init__(self, **kwargs):
+        super(Decoder, self).__init__(**kwargs)
+        self.lstm = layers.LSTM(700, dropout=0.5)
+
+    def call(self, inputs):
+        x = self.lstm(inputs)
+        return x
 
 class VAE(keras.Model):
     def __init__(self, encoder, decoder, **kwargs):
@@ -49,8 +56,8 @@ class VAE(keras.Model):
         if isinstance(data, tuple):
             data = data[0]
         with tf.GradientTape() as tape:
-            z_mean, z_log_var, z = encoder(data)
-            reconstruction = decoder(z)
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
             reconstruction_loss = tf.reduce_mean(
                 keras.losses.binary_crossentropy(data, reconstruction)
             )
