8baeade09065ab9ec45be69f21eb1159db54cc17
Isaac Buitrago <isaacb@arlut.utexas.edu>
2020-11-21 23:04:24 -0600

    Creating embedding layer

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import os
 import string
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.layers import Embedding
+import tensorflow.keras.preprocessing as pre
 
+# max chars in tweet
+TWEET_LEN = 280
+trainData = './data/dril.train.txt'
 
 # load vocab
-with open('./data/vocab.txt', 'r') as file:
+with open('./data/vocab.txt', 'r', encoding='UTF-8') as file:
     vocab = file.read().split()
     vocab = set(vocab)
 
@@ -22,3 +26,24 @@ def cleanTweets(doc, vocab):
     tokens = ' '.join(tokens)
     return tokens
 
+# load training data
+with open(trainData, 'r', encoding='UTF-8') as file:
+    X_train = file.read()
+
+# clean training data
+X_train = cleanTweets(X_train, vocab)
+
+# map words to integers
+tokenizer = pre.text.Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+# encode data
+encodedTweets = tokenizer.texts_to_sequences(X_train)
+
+X_train = pre.sequence.pad_sequences(encodedTweets, maxlen=TWEET_LEN, padding='post')
+vocabSize = len(tokenizer.word_index) + 1
+
+print(X_train)
+
+# create embedding layer
+embedding = Embedding(input_dim=vocabSize, output_dim=100, input_length=TWEET_LEN)
