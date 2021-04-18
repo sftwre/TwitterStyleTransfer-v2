4f59b3e3b09c71ccaf7effb6df4150a4295dd07c
Isaac Buitrago <mightyking7@gmail.com>
2021-04-17 19:37:22 -0500

    moved bash scripts into seperate folder and renamed Job name

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export BUCKET_NAME=torch_model_info
-export JOB_NAME=train_vae
+export JOB_NAME=pytorch_job_$(date +%Y%m%d_%H%M%S)
 export JOB_DIR=gs://${BUCKET_NAME}/${JOB_NAME}/models
