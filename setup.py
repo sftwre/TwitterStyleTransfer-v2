77e474624a877c27c6f1e3bb5fa416f81c70addd
Isaac Buitrago <mightyking7@gmail.com>
2021-04-20 19:11:02 -0500

    updated requirements and loaded dependencies from it in setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import find_packages
 from setuptools import setup
-from pip.req import parse_requirements
 
-REQUIRED_PACKAGES = [
-    'google-cloud-storage>=1.14.0',
-    'pandas>=0.23.4',
-    'tensorboard==2.4.0',
-    'torchtext==0.8.0',
-    'nltk==3.5'
-]
+
+with open('./requirements.txt') as f:
+    REQUIRED_PACKAGES = f.read().splitlines()
+
 
 setup(
     name='trainer',
